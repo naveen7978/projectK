31a645092d77cd2a835093ef8364c37829e2f514
Naveen <naveentanniru86@gmail.com>
2025-09-18 21:56:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+const port = 3000 
+app.get('/',(req,res)=>{
+    res.send('Hello World!')
+})
+app.get('/twitter',(req,res)=>{
+    res.send('this is my twitter account')
+})
+app.get('/login',(req,res)=>{
+    res.send('<h1>please login <h1>')
+})
+app.listen(process.env.PORT,()=>{
+    console.log(`app listening on port ${port}`)
+})
